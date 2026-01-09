ba95ede32f5bfbcfc81b4f4f4a0c8b86b5d30c73
shahjaynish <shahjaynnish1225@gmail.com>
2026-01-09 16:40:34 +0530

    Added publishing code

--- audiovisualizer/build.gradle.kts
+++ audiovisualizer/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -42,3 +43,24 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+
+                from(components["release"])
+
+                // CHANGE THIS TO YOUR USERNAME
+                groupId = "com.github.Excelsior-Technologies-Community"
+
+                // THIS CAN BE YOUR LIBRARY NAME
+                artifactId = "audiovisualizer"
+
+                // This can be anything for now
+                version = "1.0.0"
+            }
+        }
+    }
+}
+
